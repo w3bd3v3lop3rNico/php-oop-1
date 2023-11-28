a3af83a9901ea79b28a7988cf0aa0bc5287daf5d
Nicola Tabai <nicola.tabai98@gmail.com>
2023-11-28 18:22:57 +0100

    basic style, fill the html with data, tests with vars and methods

--- index.php
+++ index.php
@@ -3,12 +3,14 @@
 class Production {
     public $title;
     public $language;
-    public $rating;
+    private $rating;
 
     function __construct(String $_title, String $_language) {
         $this->title = $_title;
         $this->language = $_language;
-        // $this->rating = $_rating;
+        $this->rating = 0; 
+        //$this->rating = $rating; // O cosi
+        //$this->setRating($rating); // O cosi
     }
 
     public function setRating($_rating) {
@@ -30,21 +32,32 @@ $split = new Production("Split", "EN");
 $split->title = "Split";
 $split->language = "EN";
 $split->setRating('5');
-// var_dump($split);
-// echo($split->rating);
+
+var_dump($split);
+
 
 $theTrumanShow = new Production("The Truman Show", "EN");
 $theTrumanShow->setRating(5);
-// var_dump($theTrumanShow);
-// echo($theTrumanShow->getRating());
-
-$WatchOutWereMad->setRating(7)
+var_dump($theTrumanShow);
 
 $movies = [
-    $split,
-    $theTrumanShow,
-    new Production("Watch Out, We're Mad!", "EN");
-]
+    new Production("Split", "EN"),
+    new Production("The Truman Show", "EN"),
+    new Production("Watch Out, We're Mad!", "EN")
+];
+
+$watchOutWereMad = $movies[2];
+$watchOutWereMad->setRating(7);
+
+$split = $movies[0];
+$split->setRating(5);
+
+$theTrumanShow = $movies[1];
+$theTrumanShow->setRating(6);
+
+
+
+
 ?>
 
 <!DOCTYPE html>
@@ -53,13 +66,26 @@ $movies = [
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>OOP-1</title>
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <header>
         <section class="container">
-
+            <h1 class="title">Movies</h1>
         </section>
     </header>
+    <main>
+        <section class="container">
+            <ul>
+                <?php foreach($movies as $movie) { ?>
+                    <li>
+                        <?php echo($movie->title . ' - ' . $movie->language . ' - ' . $movie->getRating() ) ?>
+                    </li>
+                <?php } ?>
+                
+            </ul>
+        </section>
+    </main>
     
 </body>
 </html>
